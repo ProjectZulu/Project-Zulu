ff0bc2464aef1c228d9b05258158cab206484e63
Tarion <Tarion@Crudedragos>
2013-02-08 22:43:15 -0400

    Upped version number.

--- src/projectzulu/common/core/DefaultProps.java
+++ src/projectzulu/common/core/DefaultProps.java
@@ -5,7 +5,7 @@ public class DefaultProps {
 	
 	/* ModIDs, Dependencies, and Version */
 	public static final String DesiredBeforeMod = "after:ExtrabiomesXL@"; // TODO: Experimental, Needs to BE Tested
-	public static final String VERSION_STRING = "0.6.2";
+	public static final String VERSION_STRING = "0.6.3";
 	public static final String CoreModId = "ProjectZulu|Core";
 	public static final String BlocksModId = "ProjectZulu|Blocks";
 	public static final String MobsModId = "ProjectZulu|Mobs";
