90c7921d0ab4da409db4da1b9c4e0c0f47aacd33
Tarion <ProjectZulu@outlook.com>
2013-03-10 12:42:00 -0300

    Use the newly Created CoconutHandler instead of the old one.

--- src/projectzulu/common/blocks/ItemBlockRecipeManager.java
+++ src/projectzulu/common/blocks/ItemBlockRecipeManager.java
@@ -49,7 +49,7 @@ public class ItemBlockRecipeManager {
 		
 		/* Coconut */
 		if(ItemList.coconutSeed.isPresent() && ItemList.coconutItem.isPresent()){
-			GameRegistry.registerCraftingHandler(coconutItemCraftingHandler);
+			GameRegistry.registerCraftingHandler(new CoconutCraftingHandler());
 		}
 		addRecipe(new OptionalItemStack(Item.bowlEmpty), new String[]{"   ","X X"," X "}, 'X', new OptionalItemStack(ItemList.coconutShell));
 		addRecipe(new OptionalItemStack(ItemList.coconutSeed), new String[]{" X "," Y ","   "}, 
@@ -303,43 +303,4 @@ public class ItemBlockRecipeManager {
 		}
 		GameRegistry.addRecipe(itemResult, componentList);
 	}
-	
-	static ICraftingHandler coconutItemCraftingHandler = new ICraftingHandler() {
-		@Override
-		public void onSmelting(EntityPlayer player, ItemStack item) {}
-
-		@Override
-		public void onCrafting(EntityPlayer player, ItemStack item,
-				IInventory craftMatrix) {
-
-			/*
-			 * This onCrafting Handle is for Sword and Coconut Recipe
-			 * 0-1-2
-			 * 3-4-5
-			 * 6-7-8
-			 */
-			ItemStack shouldBeSword = craftMatrix.getStackInSlot(1);
-			ItemStack shouldBeCoconut = craftMatrix.getStackInSlot(4);
-
-			if(shouldBeSword != null && shouldBeCoconut != null 
-					&& (shouldBeSword.getItem().itemID == Item.swordWood.itemID || shouldBeSword.getItem().itemID == Item.swordStone.itemID 
-					|| shouldBeSword.getItem().itemID == Item.swordSteel.itemID || shouldBeSword.getItem().itemID == Item.swordGold.itemID 
-					|| shouldBeSword.getItem().itemID == Item.swordDiamond.itemID) 
-					&& ItemList.coconutItem.isPresent() && shouldBeCoconut.getItem() == ItemList.coconutItem.get()){
-				//Put Sword Back
-				ItemStack tempItem1 = new ItemStack(shouldBeSword.getItem(), 2, shouldBeSword.getItemDamage()+1);
-				craftMatrix.setInventorySlotContents(1, tempItem1);
-				//Place Coconut Milk
-				if(ItemList.coconutMilkFragment.isPresent()){
-					ItemStack tempItem2 = new ItemStack(ItemList.coconutMilkFragment.get(), 2);
-					craftMatrix.setInventorySlotContents(3, tempItem2);
-				}
-				//Place Coconut Shell
-				if(ItemList.coconutShell.isPresent()){
-					ItemStack tempItem3 = new ItemStack(ItemList.coconutShell.get(), 2);
-					craftMatrix.setInventorySlotContents(5, tempItem3);
-				}
-			}
-		}
-	};
 }
