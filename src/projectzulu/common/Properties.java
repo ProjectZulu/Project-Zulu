dcca1227c3cb31166b91af02b4f5b098395dfe4c
Tarion <ProjectZulu@outlook.com>
2013-05-14 17:24:02 -0300

    Fix issue with certain properties not reading/writing to configuration

--- src/projectzulu/common/Properties.java
+++ src/projectzulu/common/Properties.java
@@ -16,7 +16,7 @@ public class Properties {
     public static void loadFromConfig(File modConfigDirectoryFile) {
         Configuration config = new Configuration(new File(modConfigDirectoryFile, DefaultProps.configDirectory
                 + DefaultProps.defaultConfigFile));
-
+        config.load();
         tombstoneOnDeath = config.get("General Controls", "Drop Tombstone On Death", tombstoneOnDeath).getBoolean(
                 tombstoneOnDeath);
         replaceFlowerPot = config.get("General Controls", "Replace Default Flower Pot", replaceFlowerPot).getBoolean(
@@ -27,5 +27,6 @@ public class Properties {
         namePlateScale = (float) config.get("MOB CONTROLS", "namePlateScale", namePlateScale).getDouble(namePlateScale);
         namePlateOpacity = (float) config.get("MOB CONTROLS", "namePlateOpacity", namePlateOpacity).getDouble(
                 namePlateScale);
+        config.save();
     }
 }
