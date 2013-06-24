348f26ce0c47d3c96b6861fbc7291358e34528b1
Tarion <ProjectZulu@outlook.com>
2013-06-23 22:29:24 -0300

    Fix issue with accessing Curse and Invisibility SubItems via reflection.

--- src/projectzulu/common/potion/subitem/SubItemPotionCurse.java
+++ src/projectzulu/common/potion/subitem/SubItemPotionCurse.java
@@ -7,7 +7,7 @@ import com.google.common.base.Optional;
 
 public class SubItemPotionCurse extends SubItemPotionGeneric {
 
-    SubItemPotionCurse(int itemID, int subID) {
+    public SubItemPotionCurse(int itemID, int subID) {
         super(itemID, subID, "Curse");
         setSubItemBounds(4, 4, 4, 0);
         setEffectScale(20 * 10, 20 * 10, 20 * 15, 20 * 20, 2);
--- src/projectzulu/common/potion/subitem/SubItemPotionInvisibility.java
+++ src/projectzulu/common/potion/subitem/SubItemPotionInvisibility.java
@@ -6,7 +6,7 @@ import com.google.common.base.Optional;
 
 public class SubItemPotionInvisibility extends SubItemPotionGeneric {
 
-    SubItemPotionInvisibility(int itemID, int subID) {
+    public SubItemPotionInvisibility(int itemID, int subID) {
         super(itemID, subID, "Invisibility");
         setSubItemBounds(4, 4, 1, 0);
         setEffectScale(20 * 15, 20 * 20, 20 * 30, 20 * 40, 1);
