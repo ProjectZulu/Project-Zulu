7ec002246286d939ae285168f6cfec4aec5d91e4
Tarion <ProjectZulu@outlook.com>
2013-06-26 13:31:08 -0300

    Pyramid chest and spawner customization options identical to Labyrinth.

--- src/projectzulu/common/world/features/PyramidFeature.java
+++ src/projectzulu/common/world/features/PyramidFeature.java
@@ -1,20 +1,37 @@
 package projectzulu.common.world.features;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.Random;
 
 import net.minecraft.block.material.Material;
 import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.util.WeightedRandom;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
+import projectzulu.common.core.DefaultProps;
 import projectzulu.common.core.ProjectZuluLog;
 import projectzulu.common.core.TerrainFeatureHelper;
 import projectzulu.common.core.features.BiomeFeature;
 import projectzulu.common.world.WorldGenPyramid;
+import cpw.mods.fml.common.Loader;
 
 public class PyramidFeature extends BiomeFeature {
     public static final String PYRAMID = "Pyramid";
+    
+    private List<EntityEntry> entityEntries = new ArrayList<EntityEntry>();
+    public int chestLootChance;
+    public int chestMaxLoot;
+    
+    public String getEntityEntry(Random rand) {
+        if (entityEntries.isEmpty()) {
+            return "EMPTY";
+        }
+        return ((EntityEntry) WeightedRandom.getRandomItem(rand, entityEntries)).entityname;
+    }
+    
     public PyramidFeature() {
         super(PYRAMID, Size.LARGE);
     }
@@ -23,6 +40,13 @@ public class PyramidFeature extends BiomeFeature {
     protected void loadDefaultSettings() {
         minChunkDistance = 9;
         chunksPerSpawn = 100;
+        chestLootChance = 15;
+        chestMaxLoot = -1;
+        if (Loader.isModLoaded(DefaultProps.MobsModId)) {
+            entityEntries.add(new EntityEntry(DefaultProps.CoreModId.concat(".Entity Mummy"), 4));
+        } else {
+            entityEntries.add(new EntityEntry("Zombie", 4));
+        }
     }
 
     @Override
