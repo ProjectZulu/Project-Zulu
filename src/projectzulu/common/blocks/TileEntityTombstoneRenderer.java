dff617c298c2b81caca97946816b16cde5f62f6a
Tarion <ProjectZulu@outlook.com>
2013-07-10 19:19:21 -0300

    Fix Scrap meat declaration, again. Fix Tombstone error caused by words larger than maxLineSize. Closes #57. Tombstone text size decreased. Worlds per line increased to 13, max lines increased to 7.

--- src/projectzulu/common/blocks/TileEntityTombstoneRenderer.java
+++ src/projectzulu/common/blocks/TileEntityTombstoneRenderer.java
@@ -30,7 +30,7 @@ public class TileEntityTombstoneRenderer extends TileEntitySpecialRenderer
             rotation = par1TileEntityTombstone.getBlockMetadata()*360/8;
         }
         
-        this.bindTextureByName(DefaultProps.blockDiretory+"Tombstone.png");
+        this.bindTextureByName(DefaultProps.blockDiretory + "Tombstone.png");
         
         GL11.glPushMatrix();
         GL11.glDisable(GL11.GL_CULL_FACE);
@@ -56,10 +56,10 @@ public class TileEntityTombstoneRenderer extends TileEntitySpecialRenderer
         GL11.glRotatef(rotation, 0.0F, 1.0F, 0.0F); // Var11 is getMetaData Rotation
         
         FontRenderer var17 = this.getFontRenderer();
-        float var12 = 0.016666668F * var10;
+        float var12 = 0.016666668F * 0.8f * var10;
         float xOffset = 0.0f;
-	    float zOffset = 0.18f;
-	    float yOffset = -0.35f;
+	    float zOffset = 0.15f;
+	    float yOffset = -0.33f;
         GL11.glTranslatef(0.0F + xOffset, 0.5F * var10 + yOffset, 0.07F * var10 + zOffset);
         GL11.glScalef(var12, -var12, var12);
         GL11.glNormal3f(0.0F, 0.0F, -1.0F * var12);
@@ -68,11 +68,10 @@ public class TileEntityTombstoneRenderer extends TileEntitySpecialRenderer
 
         for (int var14 = 0; var14 < par1TileEntityTombstone.signText.length; ++var14){
             String var15 = par1TileEntityTombstone.signText[var14];
-            if (var14 == par1TileEntityTombstone.lineBeingEdited){
+            if (var14 == par1TileEntityTombstone.lineBeingEdited) {
                 var15 = "> " + var15 + " <";
                 var17.drawString(var15, -var17.getStringWidth(var15) / 2, var14 * 10 - par1TileEntityTombstone.signText.length * 5, var13);
-            }
-            else{
+            } else {
                 var17.drawString(var15, -var17.getStringWidth(var15) / 2, var14 * 10 - par1TileEntityTombstone.signText.length * 5, var13);
             }
         }
@@ -85,6 +84,7 @@ public class TileEntityTombstoneRenderer extends TileEntitySpecialRenderer
         return new StringBuffer(s).reverse().toString();
     }
     
+    @Override
     public void renderTileEntityAt(TileEntity par1TileEntity, double par2, double par4, double par6, float par8){
         this.renderTileEntityTombstoneAt((TileEntityTombstone)par1TileEntity, par2, par4, par6, par8);
     }
