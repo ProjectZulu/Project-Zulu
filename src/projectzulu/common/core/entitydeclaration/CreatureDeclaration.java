af3071ba707c1432b6044ee749d1b097e3dd5ee4
Tarion <ProjectZulu@outlook.com>
2013-07-30 23:45:14 -0300

    Code format. Cause its ugly.

--- src/projectzulu/common/core/entitydeclaration/CreatureDeclaration.java
+++ src/projectzulu/common/core/entitydeclaration/CreatureDeclaration.java
@@ -2,7 +2,6 @@ package projectzulu.common.core.entitydeclaration;
 
 import java.io.File;
 
-import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraftforge.common.Configuration;
@@ -22,97 +21,112 @@ import cpw.mods.fml.common.registry.LanguageRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public abstract class CreatureDeclaration implements EntityDeclaration{
-	protected String mobName;
-	protected Class mobClass;
-	protected EnumCreatureType enumCreatureType;
-	protected boolean shouldExist = true;
-	protected boolean reportSpawningInLog = false;
-	protected int maxSpawnInChunk = 4;
-	
-	protected int trackingRange;
-	protected int updateFrequency;
-	protected boolean sendsVelocityUpdates;
-	protected boolean shouldDespawn;
-	
-	protected int minDropNum = 0;
-	protected int maxDropNum = 0;
-	
-	protected CreatureDeclaration(String mobName, Class mobClass, EnumCreatureType creatureType){
-		this.mobName = mobName;
-		this.mobClass = mobClass;
-		this.enumCreatureType = creatureType;
-		shouldDespawn = enumCreatureType == EnumCreatureType.creature ? false : true;
-	}
+public abstract class CreatureDeclaration implements EntityDeclaration {
+    protected String mobName;
+    protected Class mobClass;
+    protected EnumCreatureType enumCreatureType;
+    protected boolean shouldExist = true;
+    protected boolean reportSpawningInLog = false;
+    protected int maxSpawnInChunk = 4;
 
-	protected void setDropAmount(int minDropNum, int maxDropNum){
-	    this.minDropNum = minDropNum;
-	    this.maxDropNum = maxDropNum;
-	}
-	
-	protected void setRegistrationProperties(int trackingRange, int updateFrequency, boolean sendsVelocityUpdates){
-		this.trackingRange = trackingRange;
-		this.updateFrequency = updateFrequency;
-		this.sendsVelocityUpdates = sendsVelocityUpdates;
-	}
-	
-	@Override
-	public boolean shouldExist() {
-		return shouldExist;
-	}
-	
-	@Override
-	public void loadCreaturesFromConfig(Configuration config) {
-		shouldExist = config.get("MOB CONTROLS."+mobName, mobName.toLowerCase()+" shouldExist", shouldExist).getBoolean(shouldExist);
-		reportSpawningInLog = config.get("MOB CONTROLS."+mobName, mobName.toLowerCase()+" reportSpawningInLog", reportSpawningInLog).getBoolean(reportSpawningInLog);
-		updateFrequency = config.get("MOB CONTROLS."+mobName, mobName.toLowerCase()+" UpdateFrequency", updateFrequency).getInt(updateFrequency);
-		maxSpawnInChunk = config.get("MOB CONTROLS."+mobName, "Max Pack Size", maxSpawnInChunk).getInt(maxSpawnInChunk);
-	}
-	
-	@Override
-	public void loadBiomesFromConfig(Configuration config) {}
+    protected int trackingRange;
+    protected int updateFrequency;
+    protected boolean sendsVelocityUpdates;
+    protected boolean shouldDespawn;
 
-	@Override
-	public void registerEntity() {
-		EntityRegistry.registerModEntity(mobClass, mobName, ProjectZulu_Core.getNextDefaultEntityID(),
-				ProjectZulu_Core.modInstance, trackingRange, updateFrequency, true);
-		LanguageRegistry.instance().addStringLocalization("entity.".concat(DefaultProps.CoreModId).concat(".").concat(mobName).concat(".name"), "en_US", mobName);		
-	}
-	
-	@Override
-	public void loadCustomMobData(File configDirectory) {
-		Configuration config = new Configuration(  new File(configDirectory, DefaultProps.configDirectory + DefaultProps.mobBiomeSpawnConfigFile) );
-		config.load();
-		CustomMobData customMobData = new CustomMobData(mobName);
-		outputDataToList(config, customMobData);	
-		CustomEntityList customEntity = CustomEntityList.getByName(mobName);
-		if(customEntity != null)
-			customEntity.modData = Optional.of(customMobData);
-		else
-			ProjectZuluLog.severe("Entity %s does not have an Entry in the CustomEntityList", mobName);
-				
-		config.save();
-	}
-	/* Create loadCustomMobData() method which calls outputData to List. loadCustom contains calls that are the same for all creatures */
-	public void outputDataToList(Configuration config, CustomMobData customMobData){
-		customMobData.reportSpawningInLog = reportSpawningInLog;
-		customMobData.creatureType = ConfigHelper.configGetCreatureType(config, "MOB CONTROLS."+mobName, "Creature Type", enumCreatureType);
-		customMobData.shouldDespawn = config.get("MOB CONTROLS."+mobName, mobName+" Should Despawn", shouldDespawn).getBoolean(true);
-		ConfigHelper.userItemConfigRangeToMobData(config, "MOB CONTROLS."+mobName, customMobData);
-		customMobData.maxSpawnInChunk = maxSpawnInChunk;
-		customMobData.minDropNum = config.get("MOB CONTROLS."+mobName, "Items to Drop Min", minDropNum).getInt(minDropNum);
-		customMobData.maxDropNum = config.get("MOB CONTROLS."+mobName, "Items to Drop Max", maxDropNum).getInt(maxDropNum);
-	}
-	
-	@Override
-	public void registerEgg() {}
-	
-	@Override
-	public void addSpawn() {}
-	
+    protected int minDropNum = 0;
+    protected int maxDropNum = 0;
+
+    protected CreatureDeclaration(String mobName, Class mobClass, EnumCreatureType creatureType) {
+        this.mobName = mobName;
+        this.mobClass = mobClass;
+        this.enumCreatureType = creatureType;
+        shouldDespawn = enumCreatureType == EnumCreatureType.creature ? false : true;
+    }
+
+    protected void setDropAmount(int minDropNum, int maxDropNum) {
+        this.minDropNum = minDropNum;
+        this.maxDropNum = maxDropNum;
+    }
+
+    protected void setRegistrationProperties(int trackingRange, int updateFrequency, boolean sendsVelocityUpdates) {
+        this.trackingRange = trackingRange;
+        this.updateFrequency = updateFrequency;
+        this.sendsVelocityUpdates = sendsVelocityUpdates;
+    }
+
+    @Override
+    public boolean shouldExist() {
+        return shouldExist;
+    }
+
+    @Override
+    public void loadCreaturesFromConfig(Configuration config) {
+        shouldExist = config.get("MOB CONTROLS." + mobName, mobName.toLowerCase() + " shouldExist", shouldExist)
+                .getBoolean(shouldExist);
+        reportSpawningInLog = config.get("MOB CONTROLS." + mobName, mobName.toLowerCase() + " reportSpawningInLog",
+                reportSpawningInLog).getBoolean(reportSpawningInLog);
+        updateFrequency = config.get("MOB CONTROLS." + mobName, mobName.toLowerCase() + " UpdateFrequency",
+                updateFrequency).getInt(updateFrequency);
+    }
+
+    @Override
+    public void loadBiomesFromConfig(Configuration config) {
+    }
+
+    @Override
+    public void registerEntity() {
+        EntityRegistry.registerModEntity(mobClass, mobName, ProjectZulu_Core.getNextDefaultEntityID(),
+                ProjectZulu_Core.modInstance, trackingRange, updateFrequency, true);
+        LanguageRegistry.instance().addStringLocalization(
+                "entity.".concat(DefaultProps.CoreModId).concat(".").concat(mobName).concat(".name"), "en_US", mobName);
+    }
+
+    @Override
+    public void loadCustomMobData(File configDirectory) {
+        Configuration config = new Configuration(new File(configDirectory, DefaultProps.configDirectory
+                + DefaultProps.mobBiomeSpawnConfigFile));
+        config.load();
+        CustomMobData customMobData = new CustomMobData(mobName);
+        outputDataToList(config, customMobData);
+        CustomEntityList customEntity = CustomEntityList.getByName(mobName);
+        if (customEntity != null)
+            customEntity.modData = Optional.of(customMobData);
+        else
+            ProjectZuluLog.severe("Entity %s does not have an Entry in the CustomEntityList", mobName);
+
+        config.save();
+    }
+
+    /*
+     * Create loadCustomMobData() method which calls outputData to List. loadCustom contains calls that are the same for
+     * all creatures
+     */
+    public void outputDataToList(Configuration config, CustomMobData customMobData) {
+        customMobData.reportSpawningInLog = reportSpawningInLog;
+        customMobData.creatureType = ConfigHelper.configGetCreatureType(config, "MOB CONTROLS." + mobName,
+                "Creature Type", enumCreatureType);
+        customMobData.shouldDespawn = config.get("MOB CONTROLS." + mobName, mobName + " Should Despawn", shouldDespawn)
+                .getBoolean(true);
+        ConfigHelper.userItemConfigRangeToMobData(config, "MOB CONTROLS." + mobName, customMobData);
+        customMobData.maxSpawnInChunk = maxSpawnInChunk;
+        customMobData.minDropNum = config.get("MOB CONTROLS." + mobName, "Items to Drop Min", minDropNum).getInt(
+                minDropNum);
+        customMobData.maxDropNum = config.get("MOB CONTROLS." + mobName, "Items to Drop Max", maxDropNum).getInt(
+                maxDropNum);
+    }
+
+    @Override
+    public void registerEgg() {
+    }
+
+    @Override
+    public void addSpawn() {
+    }
+
     @SideOnly(Side.CLIENT)
     public abstract RenderWrapper getEntityrender(Class<? extends EntityLivingBase> entityClass);
-	
+
     @Override
     @SideOnly(Side.CLIENT)
     public void registerModelAndRender() {
--- src/projectzulu/common/core/entitydeclaration/SpawnableDeclaration.java
+++ src/projectzulu/common/core/entitydeclaration/SpawnableDeclaration.java
@@ -10,84 +10,93 @@ import projectzulu.common.core.ConfigHelper;
 import projectzulu.common.core.ProjectZuluLog;
 import cpw.mods.fml.common.registry.EntityRegistry;
 
-public abstract class SpawnableDeclaration extends EggableDeclaration{
+public abstract class SpawnableDeclaration extends EggableDeclaration {
 
-	protected int spawnRate;
-	protected boolean useGlobalSpawn = false;
-	protected int secondarySpawnRate;
-	protected int minInChunk;
-	protected int maxInChunk;
-	protected EnumCreatureType spawnType;
+    protected int spawnRate;
+    protected boolean useGlobalSpawn = false;
+    protected int secondarySpawnRate;
+    protected int minInChunk;
+    protected int maxInChunk;
+    protected EnumCreatureType spawnType;
 
-	protected ArrayList<String> defaultBiomesToSpawn = new ArrayList<String>();	
-	ArrayList<SpawnEntry> biomesToSpawn = new ArrayList<SpawnEntry>();
-	
-	protected SpawnableDeclaration(String mobName, Class mobClass, EnumCreatureType creatureType) {
-		super(mobName, mobClass, creatureType);
-		spawnType = creatureType;
-	}
-	
-	protected void setSpawnProperties(int spawnRate, int secondarySpawnRate, int minInChunk, int maxInChunk){
-		this.spawnRate = spawnRate;
-		this.secondarySpawnRate = secondarySpawnRate;
-		this.minInChunk = minInChunk;
-		this.maxInChunk = maxInChunk;
-	}
-	
-	@Override
-	public void loadCreaturesFromConfig(Configuration config) {
-		super.loadCreaturesFromConfig(config);
-		spawnRate = config.get("MOB CONTROLS."+mobName, mobName.toLowerCase()+" SpawnRate", spawnRate).getInt(spawnRate);
-		secondarySpawnRate = config.get("MOB CONTROLS."+mobName, mobName.toLowerCase()+" SecondarySpawnRate",secondarySpawnRate).getInt(secondarySpawnRate);
-		minInChunk = config.get("MOB CONTROLS."+mobName, mobName.toLowerCase()+" minInChunk", minInChunk).getInt(minInChunk);
-		maxInChunk = config.get("MOB CONTROLS."+mobName, mobName.toLowerCase()+" maxInChunk", maxInChunk).getInt(maxInChunk);
-		spawnType = ConfigHelper.configGetCreatureType(config, "MOB CONTROLS."+mobName, "Spawn List Type", spawnType);
-		
-		useGlobalSpawn = config.get("MOB CONTROLS."+mobName, "Use Global Spawn Rates", useGlobalSpawn).getBoolean(useGlobalSpawn);
-	}
-	
-	@Override
-	public void loadBiomesFromConfig(Configuration config) {
-		for (int i = 0; i < BiomeGenBase.biomeList.length; i++) {
-			if(BiomeGenBase.biomeList[i] == null){
-				continue;
-			}
-            SpawnEntry spawnEntry = ConfigHelper.configGetSpawnEntry(config, "Mob Spawn Biome Controls." + mobName, BiomeGenBase.biomeList[i],
-                    defaultBiomesToSpawn.contains(BiomeGenBase.biomeList[i].biomeName), spawnRate, minInChunk,
-                    maxInChunk);
-            if(spawnEntry != null){
+    protected ArrayList<String> defaultBiomesToSpawn = new ArrayList<String>();
+    ArrayList<SpawnEntry> biomesToSpawn = new ArrayList<SpawnEntry>();
+
+    protected SpawnableDeclaration(String mobName, Class mobClass, EnumCreatureType creatureType) {
+        super(mobName, mobClass, creatureType);
+        spawnType = creatureType;
+    }
+
+    protected void setSpawnProperties(int spawnRate, int secondarySpawnRate, int minInChunk, int maxInChunk) {
+        this.spawnRate = spawnRate;
+        this.secondarySpawnRate = secondarySpawnRate;
+        this.minInChunk = minInChunk;
+        this.maxInChunk = maxInChunk;
+    }
+
+    @Override
+    public void loadCreaturesFromConfig(Configuration config) {
+        super.loadCreaturesFromConfig(config);
+        spawnRate = config.get("MOB CONTROLS." + mobName, mobName.toLowerCase() + " SpawnRate", spawnRate).getInt(
+                spawnRate);
+        secondarySpawnRate = config.get("MOB CONTROLS." + mobName, mobName.toLowerCase() + " SecondarySpawnRate",
+                secondarySpawnRate).getInt(secondarySpawnRate);
+        minInChunk = config.get("MOB CONTROLS." + mobName, mobName.toLowerCase() + " minInChunk", minInChunk).getInt(
+                minInChunk);
+        maxInChunk = config.get("MOB CONTROLS." + mobName, mobName.toLowerCase() + " maxInChunk", maxInChunk).getInt(
+                maxInChunk);
+        maxSpawnInChunk = config.get("MOB CONTROLS." + mobName, "Max Pack Size", maxSpawnInChunk).getInt(
+                maxSpawnInChunk);
+        spawnType = ConfigHelper.configGetCreatureType(config, "MOB CONTROLS." + mobName, "Spawn List Type", spawnType);
+        useGlobalSpawn = config.get("MOB CONTROLS." + mobName, "Use Global Spawn Rates", useGlobalSpawn).getBoolean(
+                useGlobalSpawn);
+    }
+
+    @Override
+    public void loadBiomesFromConfig(Configuration config) {
+        for (int i = 0; i < BiomeGenBase.biomeList.length; i++) {
+            if (BiomeGenBase.biomeList[i] == null) {
+                continue;
+            }
+            SpawnEntry spawnEntry = ConfigHelper.configGetSpawnEntry(config, "Mob Spawn Biome Controls." + mobName,
+                    BiomeGenBase.biomeList[i], defaultBiomesToSpawn.contains(BiomeGenBase.biomeList[i].biomeName),
+                    spawnRate, minInChunk, maxInChunk);
+            if (spawnEntry != null) {
                 biomesToSpawn.add(spawnEntry);
             }
-		}
-	}
-	
-	/* Create loadCustomMobData() method which calls outputData to List. loadCustom contains calls that are the same for all creatures */
-	@Override
-	public void outputDataToList(Configuration config, CustomMobData customMobData){
-	    super.outputDataToList(config, customMobData);
-	    customMobData.secondarySpawnRate = secondarySpawnRate;
-	    customMobData.spawnType = spawnType;
-	}
-	
-	@Override
-	public void addSpawn() {
-		if(spawnType == null){
-			return;
-		}
-		
-		for (int i = 0; i < biomesToSpawn.size(); i++){
-		    if(useGlobalSpawn){
+        }
+    }
+
+    /*
+     * Create loadCustomMobData() method which calls outputData to List. loadCustom contains calls that are the same for
+     * all creatures
+     */
+    @Override
+    public void outputDataToList(Configuration config, CustomMobData customMobData) {
+        super.outputDataToList(config, customMobData);
+        customMobData.secondarySpawnRate = secondarySpawnRate;
+        customMobData.spawnType = spawnType;
+    }
+
+    @Override
+    public void addSpawn() {
+        if (spawnType == null) {
+            return;
+        }
+
+        for (int i = 0; i < biomesToSpawn.size(); i++) {
+            if (useGlobalSpawn) {
                 EntityRegistry.addSpawn(mobClass, spawnRate, minInChunk, maxInChunk, spawnType,
                         biomesToSpawn.get(i).biome);
-		    }else{
-	            EntityRegistry.addSpawn(mobClass, biomesToSpawn.get(i).spawnRate, biomesToSpawn.get(i).minInChunk,
-	                    biomesToSpawn.get(i).maxInChunk, spawnType, biomesToSpawn.get(i).biome);
-		    }
-			if(reportSpawningInLog){
+            } else {
+                EntityRegistry.addSpawn(mobClass, biomesToSpawn.get(i).spawnRate, biomesToSpawn.get(i).minInChunk,
+                        biomesToSpawn.get(i).maxInChunk, spawnType, biomesToSpawn.get(i).biome);
+            }
+            if (reportSpawningInLog) {
                 ProjectZuluLog.info("Registering %s to biome %s at rates of %s,%s,%s", mobClass.getSimpleName(),
                         biomesToSpawn.get(i).biome.biomeName, biomesToSpawn.get(i).spawnRate,
                         biomesToSpawn.get(i).minInChunk, biomesToSpawn.get(i).maxInChunk);
-			}
-		}
-	}
+            }
+        }
+    }
 }
