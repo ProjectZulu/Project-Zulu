1dd831536491ace88fccea4abf8991e76f08dc8c
Tarion <ProjectZulu@outlook.com>
2013-08-02 21:21:46 -0300

    Fix error with aramdillo render: not overriding appropriate function. Add appropriate @override to other functions.

--- src/projectzulu/common/mobs/models/ModelArmadillo.java
+++ src/projectzulu/common/mobs/models/ModelArmadillo.java
@@ -4,10 +4,12 @@ import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.MathHelper;
 
 import org.lwjgl.opengl.GL11;
 
+import projectzulu.common.core.ProjectZuluLog;
 import projectzulu.common.mobs.entity.EntityArmadillo;
 import projectzulu.common.mobs.entity.EntityStates;
 
@@ -153,7 +155,7 @@ public class ModelArmadillo extends ModelBase {
 	setRotation(leg1, 0F, 0F, 0F);
 
     }
-
+    @Override
     public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
 	super.render(entity, f, f1, f2, f3, f4, f5);
 	setRotationAngles(f, f1, f2, f3, f4, f5, entity);
@@ -176,7 +178,7 @@ public class ModelArmadillo extends ModelBase {
 	    WHOLE.render(f5);
 	}
     }
-
+    @Override
     public void setRotationAngles(float par1, float par2, float par3, float par4, float par5, float par6,
 	    Entity par7Entity) {
 	EntityArmadillo var5 = (EntityArmadillo) par7Entity;
@@ -192,7 +194,8 @@ public class ModelArmadillo extends ModelBase {
 	}
     }
 
-    public void setLivingAnimations(EntityLiving par1EntityLiving, float par2, float par3, float par4) {
+    @Override
+    public void setLivingAnimations(EntityLivingBase par1EntityLiving, float par2, float par3, float par4) {
 
 	EntityArmadillo var5 = (EntityArmadillo) par1EntityLiving;
 
@@ -245,6 +248,7 @@ public class ModelArmadillo extends ModelBase {
 	    leg3.rotationPointZ = var5.eleg3.translateZ(-0.5f * animSpeed, -4f, 4f);
 	    leg4.rotationPointZ = var5.eleg4.translateZ(-0.5f * animSpeed, -4f, 4f);
 	} else if (var5.getEntityState() != EntityStates.inCover && var5.isCharging() == false) {
+	    ProjectZuluLog.info("Is Running?");
 	    float animSpeed = 2.0f;
 	    /* Make Complete Body Circle */
 	    WHOLE.rotateAngleX = var5.eWHOLE.rotateX(-0.09f * animSpeed, 0f, (float) ((390 + 360) * Math.PI / 180f));
