fe3c7e8ea9c1cd9911028d6660c83101a0ecbdd6
Tarion <ProjectZulu@outlook.com>
2013-11-06 22:41:04 -0400

    Fix tombstone Loot Indicators not animating when gamerule doDayLightCycle is disabled. Closes #76

--- src/projectzulu/common/blocks/tombstone/TileEntityTombstoneRenderer.java
+++ src/projectzulu/common/blocks/tombstone/TileEntityTombstoneRenderer.java
@@ -1,12 +1,9 @@
 package projectzulu.common.blocks.tombstone;
 
-import net.minecraft.block.Block;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.item.EntityXPOrb;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 
@@ -14,73 +11,72 @@ import org.lwjgl.opengl.GL11;
 
 import projectzulu.common.blocks.ModelTombstone;
 import projectzulu.common.core.DefaultProps;
+import projectzulu.common.core.ProjectZuluLog;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-public class TileEntityTombstoneRenderer extends TileEntitySpecialRenderer
-{
+public class TileEntityTombstoneRenderer extends TileEntitySpecialRenderer {
     /** The ModelSign instance used by the TileEntityTombstoneRenderer */
     private ModelTombstone modelSign = new ModelTombstone();
     public static final ResourceLocation TOMBSTONE = new ResourceLocation(DefaultProps.blockKey, "Tombstone.png");
-    
-    public void renderTileEntityTombstoneAt(TileEntityTombstone par1TileEntityTombstone, double par2, double par4, double par6, float par8){
-        Block var9 = par1TileEntityTombstone.getBlockType();
+
+    public void renderTileEntityTombstoneAt(TileEntityTombstone par1TileEntityTombstone, double par2, double par4,
+            double par6, float par8) {
         int meta;
         float rotation;
-        if(par8 == -1){
+        if (par8 == -1) {
             meta = 4;
-            rotation = meta*360/8;
-        }else{
+            rotation = meta * 360 / 8;
+        } else {
             meta = par1TileEntityTombstone.getBlockMetadata();
-            rotation = par1TileEntityTombstone.getBlockMetadata()*360/8;
+            rotation = par1TileEntityTombstone.getBlockMetadata() * 360 / 8;
         }
-        
-       
-        
         this.bindTexture(TOMBSTONE);
-        
+
         GL11.glPushMatrix();
         GL11.glDisable(GL11.GL_CULL_FACE);
         GL11.glEnable(GL11.GL_DEPTH_TEST);
         GL11.glRotatef(0.0f, 0.0f, 1.0F, 0.0f);
-        GL11.glTranslatef((float)par2+0.5f, (float)par4+0.88f, (float)par6+0.5f);
+        GL11.glTranslatef((float) par2 + 0.5f, (float) par4 + 0.88f, (float) par6 + 0.5f);
         GL11.glScalef(-0.9f, -0.8F, 0.9F);
-        this.modelSign.render1((Entity)null, 0, 0, 0, 0, 0, 0.0459f, -rotation);
+        this.modelSign.render1((Entity) null, 0, 0, 0, 0, 0, 0.0459f, -rotation);
         GL11.glPopMatrix();
-        
+
         GL11.glPushMatrix();
         GL11.glDisable(GL11.GL_CULL_FACE);
         GL11.glEnable(GL11.GL_DEPTH_TEST);
         GL11.glRotatef(0.0f, 0.0f, 1.0F, 0.0f);
-        GL11.glTranslatef((float)par2+0.5f, (float)par4+0.88f, (float)par6+0.5f);
+        GL11.glTranslatef((float) par2 + 0.5f, (float) par4 + 0.88f, (float) par6 + 0.5f);
         GL11.glScalef(-0.899f, -0.8F, 0.899F);
-        this.modelSign.render2((Entity)null, 0, 0, 0, 0, 0, 0.0459f, -rotation);
+        this.modelSign.render2((Entity) null, 0, 0, 0, 0, 0, 0.0459f, -rotation);
         GL11.glPopMatrix();
-        
+
         GL11.glPushMatrix();
         float var10 = 0.6666667F;
-        GL11.glTranslatef((float)par2 + 0.5F, (float)par4 + 0.5F, (float)par6 + 0.5F);
+        GL11.glTranslatef((float) par2 + 0.5F, (float) par4 + 0.5F, (float) par6 + 0.5F);
         GL11.glRotatef(rotation, 0.0F, 1.0F, 0.0F); // Var11 is getMetaData Rotation
-        
+
         FontRenderer var17 = this.getFontRenderer();
         float var12 = 0.016666668F * 0.8f * var10;
         float xOffset = 0.0f;
-	    float zOffset = 0.15f;
-	    float yOffset = -0.32f;
+        float zOffset = 0.15f;
+        float yOffset = -0.32f;
         GL11.glTranslatef(0.0F + xOffset, 0.5F * var10 + yOffset, 0.07F * var10 + zOffset);
         GL11.glScalef(var12, -var12, var12);
         GL11.glNormal3f(0.0F, 0.0F, -1.0F * var12);
         GL11.glDepthMask(false);
         byte var13 = 0;
 
-        for (int var14 = 0; var14 < par1TileEntityTombstone.signText.length; ++var14){
+        for (int var14 = 0; var14 < par1TileEntityTombstone.signText.length; ++var14) {
             String var15 = par1TileEntityTombstone.signText[var14];
             if (var14 == par1TileEntityTombstone.lineBeingEdited) {
                 var15 = "> " + var15 + " <";
-                var17.drawString(var15, -var17.getStringWidth(var15) / 2, var14 * 10 - par1TileEntityTombstone.signText.length * 5, var13);
+                var17.drawString(var15, -var17.getStringWidth(var15) / 2, var14 * 10
+                        - par1TileEntityTombstone.signText.length * 5, var13);
             } else {
-                var17.drawString(var15, -var17.getStringWidth(var15) / 2, var14 * 10 - par1TileEntityTombstone.signText.length * 5, var13);
+                var17.drawString(var15, -var17.getStringWidth(var15) / 2, var14 * 10
+                        - par1TileEntityTombstone.signText.length * 5, var13);
             }
         }
         GL11.glDepthMask(true);
@@ -89,7 +85,8 @@ public class TileEntityTombstoneRenderer extends TileEntitySpecialRenderer
 
         if (par1TileEntityTombstone.getEntityOrb() != null) {
             GL11.glPushMatrix();
-            long time = par1TileEntityTombstone.worldObj.getWorldTime();
+            long time = par1TileEntityTombstone.worldObj.getWorldInfo().getWorldTotalTime();
+            ProjectZuluLog.info("Time %s", time);
             float orbOrbitRadius = 0.3f;
             float periodScale = 6.0f;
             GL11.glTranslatef((float) par2 + 0.5f, (float) (par4 + 1.1f + 0.05f * Math.cos(time / 20f)),
@@ -106,11 +103,11 @@ public class TileEntityTombstoneRenderer extends TileEntitySpecialRenderer
             GL11.glPopMatrix();
         }
     }
-    
+
     public String reverse(String s) {
         return new StringBuffer(s).reverse().toString();
     }
-    
+
     /**
      * 
      * @param time
@@ -127,7 +124,7 @@ public class TileEntityTombstoneRenderer extends TileEntitySpecialRenderer
             return orbitRadius * (float) Math.cos(time / orbitPeriod + orb * Math.PI * 2 / maxOrbs);
         }
     }
-    
+
     private float getOrbitTransformationZ(float time, int orb, int maxOrbs, float orbitRadius, float orbitPeriod) {
         if (orb <= 0) {
             return 0;
@@ -135,9 +132,9 @@ public class TileEntityTombstoneRenderer extends TileEntitySpecialRenderer
             return orbitRadius * (float) Math.sin(time / orbitPeriod + orb * Math.PI * 2 / maxOrbs);
         }
     }
-    
+
     @Override
-    public void renderTileEntityAt(TileEntity par1TileEntity, double par2, double par4, double par6, float par8){
-        this.renderTileEntityTombstoneAt((TileEntityTombstone)par1TileEntity, par2, par4, par6, par8);
+    public void renderTileEntityAt(TileEntity par1TileEntity, double par2, double par4, double par6, float par8) {
+        this.renderTileEntityTombstoneAt((TileEntityTombstone) par1TileEntity, par2, par4, par6, par8);
     }
 }
