88386bb79414922638d752f59cac4fab33f07ca7
unknown <ProjectZulu@outlook.com>
2014-01-07 12:28:37 -0400

    Add NPE to Lizard spit render. Will cause issues with Lizard Spit visability as Icon are bounded to itemblock in MC design. Module refactor will fix. Closes #87

--- src/projectzulu/common/mobs/renders/RenderLizardSpit.java
+++ src/projectzulu/common/mobs/renders/RenderLizardSpit.java
@@ -13,45 +13,48 @@ import org.lwjgl.opengl.GL12;
 import projectzulu.common.core.ItemGenerics;
 import projectzulu.common.mobs.entity.EntityLizardSpit;
 
-
-public class RenderLizardSpit extends Render implements RenderWrapper
-{
+public class RenderLizardSpit extends Render implements RenderWrapper {
     private float field_77002_a;
 
-    public RenderLizardSpit(float par1)
-    {
+    public RenderLizardSpit(float par1) {
         this.field_77002_a = par1;
     }
 
-    public void doRenderLizardSpit(EntityLizardSpit par1EntityLizardSpit, double par2, double par4, double par6, float par8, float par9)
-    {
-        GL11.glPushMatrix();
-        this.bindEntityTexture(par1EntityLizardSpit);
-        GL11.glTranslatef((float)par2, (float)par4, (float)par6);
-        GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-        float var10 = this.field_77002_a;
-        GL11.glScalef(var10 / 1.0F, var10 / 1.0F, var10 / 1.0F);
-        byte var11 = 9;
+    public void doRenderLizardSpit(EntityLizardSpit par1EntityLizardSpit, double par2, double par4, double par6,
+            float par8, float par9) {
         Icon icon = ItemGenerics.Properties.LizardSpit.getIcon();
-        Tessellator var12 = Tessellator.instance;
-        float var13 = icon.getMinU();
-        float var14 = icon.getMaxU();
-        float var15 = icon.getMinV();
-        float var16 = icon.getMaxV();
-        float var17 = 1.0F;
-        float var18 = 0.5F;
-        float var19 = 0.25F;
-        GL11.glRotatef(180.0F - this.renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
-        GL11.glRotatef(-this.renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
-        var12.startDrawingQuads();
-        var12.setNormal(0.0F, 1.0F, 0.0F);
-        var12.addVertexWithUV((double)(0.0F - var18), (double)(0.0F - var19), 0.0D, (double)var13, (double)var16);
-        var12.addVertexWithUV((double)(var17 - var18), (double)(0.0F - var19), 0.0D, (double)var14, (double)var16);
-        var12.addVertexWithUV((double)(var17 - var18), (double)(1.0F - var19), 0.0D, (double)var14, (double)var15);
-        var12.addVertexWithUV((double)(0.0F - var18), (double)(1.0F - var19), 0.0D, (double)var13, (double)var15);
-        var12.draw();
-        GL11.glDisable(GL12.GL_RESCALE_NORMAL);
-        GL11.glPopMatrix();
+        if (icon != null) { // Icon only null if ItemGenerics is disabled, TODO more elegant solution
+            GL11.glPushMatrix();
+            this.bindEntityTexture(par1EntityLizardSpit);
+            GL11.glTranslatef((float) par2, (float) par4, (float) par6);
+            GL11.glEnable(GL12.GL_RESCALE_NORMAL);
+            float var10 = this.field_77002_a;
+            GL11.glScalef(var10 / 1.0F, var10 / 1.0F, var10 / 1.0F);
+
+            Tessellator var12 = Tessellator.instance;
+            float var13 = icon.getMinU();
+            float var14 = icon.getMaxU();
+            float var15 = icon.getMinV();
+            float var16 = icon.getMaxV();
+            float var17 = 1.0F;
+            float var18 = 0.5F;
+            float var19 = 0.25F;
+            GL11.glRotatef(180.0F - this.renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
+            GL11.glRotatef(-this.renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
+            var12.startDrawingQuads();
+            var12.setNormal(0.0F, 1.0F, 0.0F);
+            var12.addVertexWithUV((double) (0.0F - var18), (double) (0.0F - var19), 0.0D, (double) var13,
+                    (double) var16);
+            var12.addVertexWithUV((double) (var17 - var18), (double) (0.0F - var19), 0.0D, (double) var14,
+                    (double) var16);
+            var12.addVertexWithUV((double) (var17 - var18), (double) (1.0F - var19), 0.0D, (double) var14,
+                    (double) var15);
+            var12.addVertexWithUV((double) (0.0F - var18), (double) (1.0F - var19), 0.0D, (double) var13,
+                    (double) var15);
+            var12.draw();
+            GL11.glDisable(GL12.GL_RESCALE_NORMAL);
+            GL11.glPopMatrix();
+        }
     }
 
     /**
